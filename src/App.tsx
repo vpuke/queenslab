92b1c00f10e507c3f941fc48d397f39ca2055e5b
viktorpuke <Viktorpuke@gmail.com>
2024-09-22 17:03:19 +0200

    FromData, flipped, errors & handleSubmit. Parsing from formSchema for validation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
-import './App.css'
+import { useState } from 'react'
+import Card from './components/Card/Card'
 import Container from './components/Container/Container'
+import { formSchema } from './utils'
+import Form from './components/Form/Form'
 
 function App() {
+  const [formData, setFormData] = useState({
+    cardNumber: '',
+    cardName: '',
+    cardMonth: '',
+    cardYear: '',
+    cardCVV: '',
+  })
+  const [isFlipped, setIsFlipped] = useState(false)
+
+  const [formErrors, setFormErrors] = useState<
+    Record<string, string | undefined>
+  >({})
+
+  function handleFormSubmit(data: typeof formData) {
+    const result = formSchema.safeParse(data)
+    if (!result.success) {
+      const errors: Record<string, string | undefined> = {}
+      result.error.errors.forEach((err) => {
+        errors[err.path[0]] = err.message
+      })
+      setFormErrors(errors)
+      console.log('errors', errors)
+    } else {
+      setFormErrors({})
+      setFormData(result.data)
+      console.log('Form submitted: ', result.data)
+    }
+  }
+
   return (
     <Container>
-      <p>Hello</p>
+      <Card formData={formData} isFlipped={isFlipped} />
+      <Form
+        onSubmit={handleFormSubmit}
+        errors={formErrors}
+        formData={formData}
+        setFormData={setFormData}
+        setIsFlipped={setIsFlipped}
+      />
     </Container>
   )
 }
