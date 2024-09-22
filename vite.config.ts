c1a8001ec25f27d2246963d0823d2ee03ad7cbbb
viktorpuke <Viktorpuke@gmail.com>
2024-09-22 18:17:58 +0200

    Vitest & Testinglibrary dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './__tests__/setup.ts',
+  },
 })
